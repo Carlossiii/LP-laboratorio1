73835cef906705301e51c01843e29b09ac4dcb1a
carlossiii <cv5673@gmail.com>
2017-08-10 10:16:52 -0300

    The main of questao01 implemented.

---
+++ src/questao01.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int coice = 0;
+
+	cout << "Calculadora de Geometria Plana e Espacial" << endl;
+	cout << "(1) Triangulo equilatero" << endl;
+	cout << "(2) Retangulo" << endl;
+	cout << "(3) Quadrado" << endl;
+	cout << "(4) Circulo" << endl;
+	cout << "(5) Piramide com base quadrangular" << endl;
+	cout << "(6) Cubo" << endl;
+	cout << "(7) Paralelepipedo" << endl;
+	cout << "(8) Esfera" << endl;
+	cout << "(0) Sair" << endl;
+	cout << "\nDigite a sua opcao:"<< endl;
+	cin >> choice >> endl;
+
+	switch (choice){
+
+	case 0:
+		break;
+
+	case 1:
+		break;
+
+	case 2:
+		break;
+
+	case 3:
+		break;
+
+	case 4:
+		break;
+
+	case 5:
+		break;
+
+	case 6:
+		break;
+
+	case 7:
+		break;
+
+	}
+
+	return 0;
+}
